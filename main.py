810fe4b02a9c8a460dd37104814e1b8a06c79140
Marcin <80240893+MarcinAn@users.noreply.github.com>
2023-04-26 20:00:28 +0200

    fix info

--- main.py
+++ main.py
@@ -1,8 +1,1 @@
-shopping_list= {}
-count = 0
-shopping_list = {'piekarnia' : ['chleb', 'bułki', 'pączek'], 'warzywniak' : ['marchew', 'seler', 'rukola']}
-print("Lista zakupów")
-for element in shopping_list:
-    print(f"Idę do {element.capitalize()} i kupuję tam {[element.capitalize() for element in shopping_list[element]]}")
-    count += len(shopping_list[element])
-print(f"W sumie kupuję {count} produktów.")
+print("Coś tam.")
